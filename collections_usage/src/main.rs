cef4259d8062538f5b10678af7b00d68f11cfda7
liberty yao <libyao@cisco.com>
2023-06-09 17:50:56 +0800

    update

--- collections_usage/src/main.rs
+++ collections_usage/src/main.rs
@@ -1,3 +1,6 @@
+use std::fs::File;
+use std::io::ErrorKind;
+
 use crate::hash_map_usage::test_hash_map;
 
 mod vector_usage;
@@ -19,4 +22,47 @@ fn main() {
     // test_iter_string();
 
     test_hash_map();
+
+    test_open_file();
+}
+
+
+fn test_open_file() {
+    let greeting_file_result = File::open("hello.txt");
+
+    let greeting_file = match greeting_file_result {
+        Ok(file) => file,
+        Err(error) => match error.kind() {
+            // 直接借用
+            ErrorKind::NotFound => match File::create("hello.txt") {
+                Ok(fc) => fc,
+                Err(e) => panic!("Problem creating the file: {:?}", e),
+            },
+            other_error => {
+                panic!("Problem opening the file: {:?}", other_error);
+            }
+        },
+    };
+}
+
+fn test_open_file2() {
+    let greeting_file = File::open("hello.txt").unwrap_or_else(|error| {
+        if error.kind() == ErrorKind::NotFound {
+            File::create("hello.txt").unwrap_or_else(|error| {
+                panic!("Problem creating the file: {:?}", error);
+            })
+        } else {
+            panic!("Problem opening the file: {:?}", error);
+        }
+    });
+}
+
+fn test_open_file3() {
+    /*
+    如果 Result 值是成员 Ok，unwrap 会返回 Ok 中的值。
+    如果 Result 是成员 Err，unwrap 会为我们调用 panic!。
+    */
+    let greeting_file = File::open("hello.txt").unwrap();
+    let greeting_file = File::open("hello.txt")
+        .expect("hello.txt should be included in this project");
 }
