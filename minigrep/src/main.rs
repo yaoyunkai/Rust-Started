95c541ae62682365ff552f72780537ab4dd47154
liberty <1052433260@qq.com>
2023-06-15 21:41:10 +0800

    program basc structure

---
+++ minigrep/src/main.rs
@@ -0,0 +1,29 @@
+/*
+std::env::args_os
+
+panic! 的调用更趋向于程序上的问题而不是使用上的问题。
+
+
+*/
+use std::env;
+use std::process;
+
+use minigrep::Config;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
+
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
+
+    if let Err(e) = minigrep::run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
+    }
+}
+
